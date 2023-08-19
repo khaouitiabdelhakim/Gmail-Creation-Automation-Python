00c9218e1d61c937344afc91623e8a39838fb643
abdelhakimkhaouiti <abdelhakimkhaouiti62@gmail.com>
2023-08-19 01:28:55 +0100

    first commit

---
+++ gmail_automation.py
@@ -0,0 +1,110 @@
+# Gmail Account Creation Automation Script - Version 1.0.0
+# Original script by Abdelhakim Khaouiti (khaouitiabdelhakim on GitHub)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.service import Service
+
+# Create a Service object with the executable path (GeckoDriver)
+service = Service('./geckodriver')
+
+# Initialize the Firefox driver with the Service object
+driver = webdriver.Firefox(service=service)
+
+# your data
+your_first_name = "Jhon"
+your_last_name = "Doe"
+your_username = "johndoe2314pro" # johndoe2314pro@gmail.com // make sure to be unique
+your_birthday = "02 3 1999" #dd m yyyy exp : 24 11 2003
+your_gender = "1" # 1:F 2:M 3:Not say 4:Custom
+your_password = "x,nscldsj123...FDKZ"
+
+try:
+    driver.get("https://accounts.google.com/signup/v2/createaccount?flowName=GlifWebSignIn&flowEntry=SignUp")
+
+    first_name = driver.find_element(By.NAME, "firstName")
+    last_name = driver.find_element(By.NAME, "lastName")
+
+    first_name.clear()
+    first_name.send_keys(your_first_name)
+
+    last_name.clear()
+    last_name.send_keys(your_last_name)
+
+    next_button = driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe")
+    next_button.click()
+
+    wait = WebDriverWait(driver, 20)
+    day = wait.until(EC.visibility_of_element_located((By.NAME, "day")))
+
+    birthday_elements = your_birthday.split()
+
+    month_dropdown = Select(driver.find_element(By.ID, "month"))
+    month_dropdown.select_by_value(birthday_elements[1])
+
+    day_field = driver.find_element(By.ID, "day")
+    day_field.clear()
+    day_field.send_keys(birthday_elements[0])
+
+    year_field = driver.find_element(By.ID, "year")
+    year_field.clear()
+    year_field.send_keys(birthday_elements[2])
+
+    gender_dropdown = Select(driver.find_element(By.ID, "gender"))
+    gender_dropdown.select_by_value(your_gender)
+
+    next_button = driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe")
+    next_button.click()
+
+    create_own_option = wait.until(EC.element_to_be_clickable((By.ID, "selectionc2")))
+    create_own_option.click()
+
+    create_own_email = wait.until(EC.element_to_be_clickable((By.NAME, "Username")))
+    username_field = driver.find_element(By.NAME, "Username")
+    username_field.clear()
+    username_field.send_keys(your_username)
+
+    next_button = driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe")
+    next_button.click()
+
+    password_field = wait.until(EC.visibility_of_element_located((By.NAME, "Passwd")))
+    password_field.clear()
+    password_field.send_keys(your_password)
+
+    password_confirmation_field = driver.find_element(By.NAME, "PasswdAgain")
+    password_confirmation_field.clear()
+    password_confirmation_field.send_keys(your_password)
+
+    next_button = driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe")
+    next_button.click()
+
+    # Skip add phone number
+    skip_button_is_visible = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button span.VfPpkd-vQzf8d")))
+    skip_button = driver.find_element(By.CSS_SELECTOR, "button span.VfPpkd-vQzf8d")
+    skip_button.click()
+
+    # Skip add recovery email
+    skip_button_is_visible = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button span.VfPpkd-vQzf8d")))
+    skip_button = driver.find_element(By.CSS_SELECTOR, "button span.VfPpkd-vQzf8d")
+    skip_button.click()
+
+    next_button = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "VfPpkd-LgbsSe")))
+    next_button.click()
+
+    # Agree on Google's privacies
+    agree_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button span.VfPpkd-vQzf8d")))
+    agree_button.click()
+
+    # Close the browser window at the end of your automation
+    driver.quit()
+
+    print("Your Gmail successfully created:\n{\ngmail: " + your_username + "@gmail.com\npassword: " + your_password + "\n}")
+
+
+except Exception as e:
+    # Close the browser window in case of failure
+    driver.quit()
+    print("Failed to create your Gmail, Sorry")
+    print(e)
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Gmail Account Creation Automation Script
+
+Automate the process of creating Gmail accounts using Selenium and Firefox WebDriver.
+
+## Overview
+
+This script allows you to automate the creation of Gmail accounts using the Selenium automation framework with the Firefox WebDriver. It navigates through the Gmail sign-up process by filling in the required details, such as name, username, password, and more.
+
+## Prerequisites
+
+- Python 3.x
+- GeckoDriver (Firefox WebDriver) - [Download GeckoDriver](https://github.com/mozilla/geckodriver/releases)
+- Selenium library - Install using `pip install selenium`
+
+## Usage
+
+1. Clone this repository:
+
+   ```
+   git clone https://github.com/khaouitiabdelhakim/gmail-automation.git
+   ```
+
+2. Download the GeckoDriver executable and place it in the repository directory.
+
+3. Install the required libraries:
+
+   ```
+   pip install selenium
+   ```
+
+4. Open the script (`gmail_automation.py`) and update the `your_first_name`, `your_last_name`, `your_username`, `your_birthday`, `your_gender`, and `your_password` variables with your desired account details.
+
+5. Run the script:
+
+   ```
+   python gmail_automation.py
+   ```
+
+6. The script will automate the Gmail account creation process and provide you with the created account's username and password at the end.
+
+7. **Important:** Please be aware that automating website interactions might go against the website's terms of use. Use this script responsibly and be cautious not to violate any rules or regulations.
+
+## Disclaimer
+
+This script is provided for educational and informational purposes only. The author is not responsible for any misuse or violation of terms of service resulting from the use of this script.
+
+## Credits
+
+Original script by Abdelhakim Khaouiti ([khaouitiabdelhakim on GitHub](https://github.com/khaouitiabdelhakim))
